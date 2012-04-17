81abd37c700102d73a748fe590af3d51071e6260
Daniel Schmidt <dschmidt@weluse.de>
2012-04-17 13:47:55 +0200

    added basic properties of the user attributes and corresponding specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  #regular expression to validate mail addresses
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,
+   :presence => true,
+   :length => { :maximum => 50 },
+   :uniqueness => { :case_sensitive => false }
+
+  validates :email,
+   :presence => true,
+   :format => { :with => email_regex },
+   :uniqueness => { :case_sensitive => false }
 end
 
 # == Schema Information
