d24e7dffdb49cc9ed89ab8d977fae972f08161db
Daniel Schmidt <dschmidt@weluse.de>
2012-04-30 09:15:19 +0200

    fixed spelling mistake at specs for user authenticate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -113,12 +113,12 @@ describe User do
     describe "autheticate method " do
 
       it "should return nil on email/password mismatch" do
-        wrong_password_user = User.autheticate(@attr[:email],"wrongpass")
+        wrong_password_user = User.authenticate(@attr[:email],"wrongpass")
         wrong_password_user.should be_nil
       end
 
       it "should return nil for an email address with no user" do
-        nonexistent_user = User.autheticate("bar@foo.com", @attr[:password])
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
         nonexistent_user.should be_nil
       end
 
