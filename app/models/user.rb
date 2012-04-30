2b61a7c661551398c67dbfa9f83ab5a085d30e94
Daniel Schmidt <dschmidt@weluse.de>
2012-04-30 09:03:31 +0200

    added password encryption, salt for a secure password and the corresponding specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :email, :name, :password, :password_confirmation
@@ -20,6 +21,34 @@ class User < ActiveRecord::Base
    :presence => true,
    :confirmation => true,
    :length => { :within => 6..40 }
+
+before_save :encrypt_password
+
+#return true if the user's pwd matches the submitted one
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+private
+
+  def encrypt_password
+    #writes the encrypted pwd in the user and adds the salt if the user is new
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+
 end
 
 # == Schema Information
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,6 +99,18 @@ describe User do
       @user.encrypted_password.should_not be_blank
     end
 
+    describe "has_password? method" do
+
+      it "should be true if the password match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("falschesPasswort").should be_false
+      end
+
+    end
+
   end
 
 end
