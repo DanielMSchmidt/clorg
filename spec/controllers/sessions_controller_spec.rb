0131e7269ec23aba53a1562a5b7397daf7e07caf
Daniel Schmidt <danielschmidt@pc228245.wlan.uni-kiel.de>
2012-05-01 17:01:33 +0200

    added specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -35,6 +35,22 @@ describe SessionsController do
         flash.now[:error].should =~ /invalid/i
       end
     end
-  end
 
+    describe "with valid email and password" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        #Fill in with tests a signed-in user
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+  end
 end
