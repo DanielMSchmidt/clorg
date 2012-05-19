32fb3fcb624d523a9d1b79935c9e7bcb8453e16e
Daniel Schmidt <dschmidt@weluse.de>
2012-05-20 01:28:55 +0200

    added functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
     redirect_to signin_path, :notice => "Please sign in to access this page!"
   end
 
+  def current_user?(user)
+    current_user == user
+  end
+
   private
 
   def user_from_remember_token
