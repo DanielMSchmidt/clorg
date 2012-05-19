8276197ae9d2784e12c10484a41b6fcdf535395a
Daniel Schmidt <dschmidt@weluse.de>
2012-05-20 01:30:12 +0200

    refactored (could save lines, because of before_filer)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,9 @@ class UsersController < ApplicationController
 
   def edit
     @title = "Edit user"
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       redirect_to @user
