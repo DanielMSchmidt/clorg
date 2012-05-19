98c5b5a73ef43cd2135c1922684a2666e00a044b
Daniel Schmidt <dschmidt@weluse.de>
2012-05-20 01:42:02 +0200

    added friendly forwarding (saving original destination of user @deny access and bringing him back to it after login)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,7 @@ module SessionsHelper
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page!"
   end
 
@@ -30,13 +31,26 @@ module SessionsHelper
     current_user == user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
 
-  def user_from_remember_token
-    User.authenticate_with_salt(*remember_token)
-  end
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token)
+    end
 
-  def remember_token
-    cookies.signed[:remember_token] || [nil,nil]
-  end
+    def remember_token
+      cookies.signed[:remember_token] || [nil,nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 end
