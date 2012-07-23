8641ef33e2ece8b685aa59f5130c8ec73b65d30e
Daniel Schmidt <dschmidt@weluse.de>
2012-07-23 21:44:20 +0200

    added bulletgem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Clorg::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Added bullet gem to find n+1 queries and so on
+
+  config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.growl = false
+  Bullet.rails_logger = true
+  Bullet.disable_browser_cache = true
+end
 end
