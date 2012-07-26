802f70e34633d4b9e39ad151c21e63a4f687354e
Daniel Schmidt <dschmidt@weluse.de>
2012-07-26 15:02:31 +0200

    added accessabilty and validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content
-  belongs_to :user, :foreign_key => :user_id
+  attr_accessible :content, :user_id, :message_id
+  has_one :user, :foreign_key => :user_id
   belongs_to :post, :foreign_key => :message_id
+
+
+  validates :content, :presence => true
+  validates :user_id, :presence => true
+  validates :message_id, :presence => true
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :content, :user_id
   has_many :comments
-	belongs_to :user, :foreign_key => :user_id  
+  belongs_to :user, :foreign_key => :user_id
+
+  validates :content, :presence => true
+  validates :user_id, :presence => true
 
 end
