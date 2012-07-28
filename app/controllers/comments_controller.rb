9ddc8b2ab8cf60523ec545d6e0dc84a3ea385eb5
Daniel Schmidt <dschmidt@weluse.de>
2012-07-28 19:04:29 +0200

    sourced find_comment out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   before_filter :get_message
+  before_filter :get_comment, :except => [:index, :create, :new]
+
   def index
     @comments = @message.comments
   end
 
   def show
-    @comment = @message.comments.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = @message.comments.find(params[:id])
   end
 
   def create
@@ -28,8 +28,6 @@ class CommentsController < ApplicationController
   end
 
   def update
-    @comment = @message.comments.find(params[:id])
-
     if @comment.update_attributes(params[:comment])
       redirect_to [@message, @comment], notice: 'Comment was successfully updated.'
     else
@@ -38,12 +36,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = @message.comments.find(params[:id])
     @comment.destroy
     redirect_to @message
   end
 
   def get_message
     @message = Message.find(params[:message_id])
+  end
+
+  def get_comment
+    @comment = @message.comments.find(params[:id])
   end
 end
