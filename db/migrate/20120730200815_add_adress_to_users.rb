2f825e8caa6d5ce02b5901dcec258ba03c302f59
Daniel Schmidt <dschmidt@weluse.de>
2012-07-30 22:21:43 +0200

    added adress colums to user

---
+++ db/migrate/20120730200815_add_adress_to_users.rb
@@ -0,0 +1,14 @@
+class AddAdressToUsers < ActiveRecord::Migration
+  def self.up
+  	add_column :users, :phone, :string
+  	add_column :users, :street, :string
+  	add_column :users, :zip, :string
+  	add_column :users, :location, :string
+  end
+  def self.down
+    remove_column :users, :phone
+  	remove_column :users, :street
+  	remove_column :users, :zip
+  	remove_column :users, :location
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120723205133) do
+ActiveRecord::Schema.define(:version => 20120730200815) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(:version => 20120723205133) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.boolean  "admin",              :default => false
+    t.string   "phone"
+    t.string   "street"
+    t.string   "zip"
+    t.string   "location"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
