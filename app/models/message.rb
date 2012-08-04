3de6f09b53b557b65e8fd53cb62b1eb7e71b910d
Daniel Schmidt <dschmidt@weluse.de>
2012-08-04 22:55:12 +0200

    added relationships

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   attr_accessible :content, :user_id
   has_many :comments
   belongs_to :user, :foreign_key => :user_id
+  has_one :tag, :foreign_key => :tag_id
   default_scope :order => 'created_at DESC'
 
   validates :content, :presence => true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730200815) do
+ActiveRecord::Schema.define(:version => 20120804204312) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(:version => 20120730200815) do
   create_table "messages", :force => true do |t|
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "tag_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
