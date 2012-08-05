f471f76f3a12ba74a766917b50da9586f4fbadc0
Daniel Schmidt <dschmidt@weluse.de>
2012-08-05 15:20:49 +0200

    prepared for ajax

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,35 +1,24 @@
 class TagsController < ApplicationController
   # GET /tags
-  # GET /tags.json
   def index
     @tags = Tag.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tags }
-    end
   end
 
   # GET /tags/1
-  # GET /tags/1.json
+  # GET /tags/1.js
   def show
     @tag = Tag.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @tag }
+      format.js #show.js.erb
     end
   end
 
   # GET /tags/new
-  # GET /tags/new.json
   def new
     @tag = Tag.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @tag }
-    end
   end
 
   # GET /tags/1/edit
@@ -38,46 +27,30 @@ class TagsController < ApplicationController
   end
 
   # POST /tags
-  # POST /tags.json
   def create
     @tag = Tag.new(params[:tag])
 
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render json: @tag, status: :created, location: @tag }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+    if @tag.save
+      redirect_to @tag, notice: 'Tag was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /tags/1
-  # PUT /tags/1.json
   def update
     @tag = Tag.find(params[:id])
-
-    respond_to do |format|
-      if @tag.update_attributes(params[:tag])
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+    if @tag.update_attributes(params[:tag])
+      redirect_to @tag, notice: 'Tag was successfully updated.'
+    else
+      format.html { render action: "edit" }
     end
   end
 
   # DELETE /tags/1
-  # DELETE /tags/1.json
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tags_url }
-      format.json { head :no_content }
-    end
+    redirect_to tags_url
   end
 end
