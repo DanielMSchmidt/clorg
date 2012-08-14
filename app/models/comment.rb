196bbf7485282416d09542b2ab7c9b62fd27e120
Daniel Schmidt <dschmidt@weluse.de>
2012-08-14 23:17:20 +0200

    fixed bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :user_id, :message_id
   has_one :user, :foreign_key => :user_id
-  belongs_to :post, :foreign_key => :message_id
+  belongs_to :message, :foreign_key => :message_id
 
 
   validates :content, :presence => true
