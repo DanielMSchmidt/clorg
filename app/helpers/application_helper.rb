f95bae77b77a5722b1a94a784bf764a0c2cb726a
david e <davide135@gmail.com>
2012-08-15 19:53:04 +0200

    merged

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	def btn_link_to(title, link, btn_typ="")
 
 		c = "btn " + btn_typ
-		content_tag :a, title, :class => c, :href => link
+		link_to  title, link, :class => c
 
 	end
 
