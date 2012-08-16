a9da7ec38e5828c964c8428051e8d912492830ff
Daniel Schmidt <dschmidt@weluse.de>
2012-08-16 22:59:47 +0200

    added validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ActiveRecord::Base
 	belongs_to :user, foreign_key: :user_id
   belongs_to :tag, foreign_key: :tag_id
 
+  #validations
+  validates :name, :start_at, :end_at, :color, :tag_id, :user_id, presence: true
+	validates :color, :hex_color => true
 
   has_event_calendar
 end
