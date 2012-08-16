e7e0b4aae48535e29fec757f36794b356c2db623
Daniel Schmidt <dschmidt@weluse.de>
2012-08-16 22:45:58 +0200

    made attributes accessible

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+	attr_accessible :name, :start_at, :end_at, :color, :tag_id, :user_id
   has_event_calendar
   belongs_to :user, foreign_key: :user_id
   belongs_to :tag, foreign_key: :tag_id
