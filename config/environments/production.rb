aa1099b9b9087217dfadcc9f0222aefdd8ec9faa
Daniel Schmidt <dschmidt@weluse.de>
2012-08-18 10:59:39 +0200

    js and css compression to false (just tst)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Clorg::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -21,7 +21,7 @@ Clorg::Application.configure do
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
+  # config.assets.manifes t = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
