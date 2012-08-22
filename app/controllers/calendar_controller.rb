b6cd50301b35adff9a2c6ba95eef2d793a68527e
Daniel Schmidt <dschmidt@weluse.de>
2012-08-22 14:13:26 +0200

    wsp

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -12,8 +12,8 @@ class CalendarController < ApplicationController
   def show
   	@year = (params[:year] || (Time.zone || Time).now.year).to_i
   	@weeknr = (params[:weeknumber] || (Time.zone || Time).now.strftime("%W")).to_i
-	monday =  Date.commercial(@year, @weeknr, 1)
-	friday =  Date.commercial(@year, @weeknr, 7)
+	  monday =  Date.commercial(@year, @weeknr, 1)
+	  friday =  Date.commercial(@year, @weeknr, 7)
   	@events = Event.where(:start_at => monday..friday)
   end
   
