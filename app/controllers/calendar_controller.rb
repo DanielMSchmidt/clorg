eb99d701d49c7324917a84830b30331d1c588444
Daniel Schmidt <dschmidt@weluse.de>
2012-08-25 15:28:31 +0200

    calendar changes on monthly calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,7 @@ class CalendarController < ApplicationController
   def index
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
+    @shown_month = Date.civil(@year, @month)
   end
 
   def show
