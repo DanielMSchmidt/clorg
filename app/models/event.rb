c6f919d17ad8e11a5bac86e8c6539697eebd3d8e
Daniel Schmidt <dschmidt@weluse.de>
2012-08-25 11:00:21 +0200

    wsp

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
-	attr_accessible :name, :start_at, :end_at, :color, :tag_id, :user_id
-	belongs_to :user, foreign_key: :user_id
+  attr_accessible :name, :start_at, :end_at, :color, :tag_id, :user_id
+  belongs_to :user, foreign_key: :user_id
   belongs_to :tag, foreign_key: :tag_id
 
   #validations
   validates :name, :start_at, :end_at, :color, :tag_id, :user_id, presence: true
-	validates :color, :hex_color => true
+  validates :color, :hex_color => true
 
   has_event_calendar
 end
