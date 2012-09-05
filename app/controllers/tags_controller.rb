ac834d4f4637dfa558bdf8d2240dfd9778f1ad2c
Daniel Schmidt <dschmidt@weluse.de>
2012-09-05 12:22:07 +0200

    deleted comments and made tags only an autogenerated method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,32 +1,21 @@
 class TagsController < ApplicationController
   before_filter :check_auth, except: :show
-  # GET /tags
   def index
     @tags = Tag.all
   end
 
-  # GET /tags/1
-  # GET /tags/1.js
   def show
     @tag = Tag.find(params[:id])
-    @messages = @tag.messages unless @tag.nil?
-    respond_to do |format|
-      format.html # show.html.erb
-      format.js #show.js.erb
-    end
   end
 
-  # GET /tags/new
   def new
     @tag = Tag.new
   end
 
-  # GET /tags/1/edit
   def edit
     @tag = Tag.find(params[:id])
   end
 
-  # POST /tags
   def create
     @tag = Tag.new(params[:tag])
 
@@ -37,7 +26,6 @@ class TagsController < ApplicationController
     end
   end
 
-  # PUT /tags/1
   def update
     @tag = Tag.find(params[:id])
     if @tag.update_attributes(params[:tag])
@@ -47,7 +35,6 @@ class TagsController < ApplicationController
     end
   end
 
-  # DELETE /tags/1
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
