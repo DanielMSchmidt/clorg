6e28cfa120cf7fa87c9121e2643fa204c4e45e06
Daniel Schmidt <dschmidt@weluse.de>
2012-09-05 12:19:54 +0200

    improved codestyle

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,24 +1,31 @@
 class CalendarController < ApplicationController
-  
-  def index
-    @month = (params[:month] || (Time.zone || Time).now.month).to_i
-    @year = (params[:year] || (Time.zone || Time).now.year).to_i
-    @shown_month = Date.civil(@year, @month)
+  before_filter :get_tags
+  before_filter :set_globals
+  # TODO: Presenter maybe?
 
-    #getting right events
+  def index
     @start = @shown_month.monday
     @ending = last_sunday(@start)
     @events = Event.where( start_at: @start..@ending)
   end
 
   def show
-  	@year = (params[:year] || (Time.zone || Time).now.year).to_i
-  	@weeknr = (params[:weeknumber] || (Time.zone || Time).now.strftime("%W")).to_i
 	  monday =  Date.commercial(@year, @weeknr, 1)
 	  friday =  Date.commercial(@year, @weeknr, 7)
-  	@events = Event.where(:start_at => monday..friday)
+  	@events = Event.where(start_at: monday..friday)
   end
-  
+
+  def get_tags
+    @tags = Tag.all
+  end
+
+  def set_globals
+    @weeknr = (params[:weeknumber] || (Time.zone || Time).now.strftime("%W")).to_i
+    @month = (params[:month] || (Time.zone || Time).now.month).to_i
+    @year = (params[:year] || (Time.zone || Time).now.year).to_i
+    @shown_month = Date.civil(@year, @month)
+  end
+
   protected
   def last_sunday(start_date)
     date = start_date + 1.month - 1.day
