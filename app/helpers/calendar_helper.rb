bae6063e7aa695ceb17d21fa7736228ab8f6cb61
Daniel Schmidt <dschmidt@weluse.de>
2012-09-25 11:13:42 +0200

    fixed rendering of day, logic into helper

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -10,4 +10,10 @@ module CalendarHelper
   	date = week_date + 1.week
   	link_to(I18n.localize(date, :format => "Woche %W"), week_calendar_path({:weeknumber => date.cweek, :year => date.cwyear}))
   end
+  def get_whole_day(day)
+    (day.to_time .. day.to_time.end_of_day)
+  end
+  def is_at_day(day, event)
+    return day.cover?(event.start_at) && day.cover?(event.end_at)
+  end
 end
