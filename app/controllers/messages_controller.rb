2a944d0f495c379587f01bb7dbb2575d34fdd930
Daniel Schmidt <dschmidt@weluse.de>
2012-10-10 14:30:11 +0200

    added endless pagination and fixed some routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,8 @@ class MessagesController < ApplicationController
   before_filter :check_auth
   
   def index
-    @tags = Tag.all
-    @messages = Message.paginate(:per_page => 25, :page => params[:page]).includes(:comments, :user)
+    #@tags = Tag.all
+    @messages = Message.page(params[:page]).per_page(10).includes(:comments, :user)
     respond_to do |format|
       format.html # show.html.erb
       format.js #show.js.erb
